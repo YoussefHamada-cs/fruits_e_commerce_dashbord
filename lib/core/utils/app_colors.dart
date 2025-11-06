60cc99aadf9d1ab1b68ad7ee7466b4dc3eb398ad
youssef hamada <yosefhagag84171@gmail.com>
2025-11-06 08:37:39 +0200

    create app colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color green = Color(0xFF1B5E37);
+  static const Color yellow = Color(0xFFf4a91f);
+  static const Color lightGreen = Color(0xFF2d9f5d);
+  static const Color darkGreen = Color(0xFF5DB957);
+  // Backgrounds
+  static const Color backgroundPrimary = Color(0xffffffff);
+  static const Color backgroundSecondary = Color(0xffe6f2f0);
+  static const Color backPrimary = Color(0xFFffffff);
+  static const Color backgroundTextField = Color(0xFFF9FAFA);
+  static const Color borderTextFormField = Color(0xFFE6E9EA);
+  static const Color textSecondary = Color(0xFF949d9e);
+  static const Color backgroundCard = Color(0xFFF3F5F7);
+  static const Color colorPrice = Color(0xFFf4a91f);
+  static const Color colorUnitPrice = Color(0xFFf8c76d);
+  static const Color black = Colors.black;
+  static const Color red = Colors.red;
+}
