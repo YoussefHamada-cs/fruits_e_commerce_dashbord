f8b24f93f49c1ddeca0a8a98b158a655be4147d1
youssef hamada <yosefhagag84171@gmail.com>
2025-11-10 07:40:55 +0200

    setup getit and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub_dashbord/core/router/app_router.dart';
+import 'package:fruits_hub_dashbord/core/services/bloc_observer.dart';
+import 'package:fruits_hub_dashbord/core/services/get_it_sevice.dart';
+import 'package:fruits_hub_dashbord/firebase_options.dart';
 
-void main() async  {
-   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  Bloc.observer = AppBlocObserver();
+  setupGetIt();
   runApp(const MyApp());
 }
 
