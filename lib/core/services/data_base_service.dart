15ba7d6e5f40f7610f9f26f78cb7dd8b8f7a2b4d
youssef hamada <yosefhagag84171@gmail.com>
2025-11-10 07:44:39 +0200

    DataBaseService

---
+++ lib/core/services/data_base_service.dart
@@ -0,0 +1,7 @@
+
+
+abstract class DataBaseService {
+Future<void> addData({required String path, required Map<String, dynamic> data,String? documentId});
+Future<Map<String, dynamic>> getData({required String path, required String documentId});
+Future<bool> checkIsUserExists({required String path, required String documentId});
+} 
