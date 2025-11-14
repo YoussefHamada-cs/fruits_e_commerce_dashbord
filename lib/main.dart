83802722e0d3971fc1788ba6118b7ad6ec5dd4c3
youssef hamada <yosefhagag84171@gmail.com>
2025-11-14 18:24:21 +0200

    setup supabase in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub_dashbord/core/router/app_router.dart';
 import 'package:fruits_hub_dashbord/core/services/bloc_observer.dart';
 import 'package:fruits_hub_dashbord/core/services/get_it_sevice.dart';
+import 'package:fruits_hub_dashbord/core/utils/end_points.dart';
 import 'package:fruits_hub_dashbord/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Supabase.initialize(url: AppKeys.url, anonKey: AppKeys.anonKey);
+//  await SupabaseStorageService.createBuckets(EndPoints.images);
   Bloc.observer = AppBlocObserver();
   setupGetIt();
   runApp(const MyApp());
