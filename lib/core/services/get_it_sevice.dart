a8d6852db98bc69ecf613546c1b8e03446a4afca
youssef hamada <yosefhagag84171@gmail.com>
2025-11-14 18:40:55 +0200

    replacing FireStorage bey SupabaseStorageService in getitservice

--- lib/core/services/get_it_sevice.dart
+++ lib/core/services/get_it_sevice.dart
@@ -4,16 +4,16 @@ import 'package:fruits_hub_dashbord/core/repos/images_repo/images_repo_impl.dart
 import 'package:fruits_hub_dashbord/core/repos/products_repo/products_repo.dart';
 import 'package:fruits_hub_dashbord/core/repos/products_repo/products_repo_impl.dart';
 import 'package:fruits_hub_dashbord/core/services/data_base_service.dart';
-import 'package:fruits_hub_dashbord/core/services/fire_storage.dart';
 import 'package:fruits_hub_dashbord/core/services/fire_store_service.dart';
 import 'package:fruits_hub_dashbord/core/services/storage_service.dart';
+import 'package:fruits_hub_dashbord/core/services/supabase_storage_service.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 void setupGetIt() {
     getIt.registerSingleton<StorageService>(
-   FireStorage()
+   SupabaseStorageService()
   );
    getIt.registerSingleton<DataBaseService>(
    FireStoreService()
