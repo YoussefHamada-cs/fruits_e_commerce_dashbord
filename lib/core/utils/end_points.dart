4e9ab07ab6e360295f6aa58917e0451ea870a13b
youssef hamada <yosefhagag84171@gmail.com>
2025-11-14 18:33:00 +0200

    EndPoints

---
+++ lib/core/utils/end_points.dart
@@ -0,0 +1,10 @@
+class EndPoints {
+  static const String images = 'images';
+  static const String addProducts = 'addproducts';
+  static const String users = 'users';
+ static const String fruitsImages ='fruits_images';
+}
+class AppKeys{
+  static const String url ='https://xufcvixjdqsoboaqtapz.supabase.co';
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1ZmN2aXhqZHFzb2JvYXF0YXB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3NjEwNzgsImV4cCI6MjA3ODMzNzA3OH0.T_OYlSeSPqiQzV1rufimpR2QoLg9QD_9XvP3sYMLOuU';
+}
